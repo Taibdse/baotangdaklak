ff01bee5dbf2fe9cf5d5ee0eb6b1235381195377
taibdse <buiductai232@gmail.com>
2020-09-03 00:56:13 +0700

    add iframe

--- js/custom/index.js
+++ js/custom/index.js
@@ -15,14 +15,13 @@ $closeFullscreenMenu.on('click', function(){
 for(var i = 0; i < $rightNav.find('> li').length; i++){
 
     $rightNav.find('> li').eq(i).on('mouseenter', function(){
-        $(this).find('img').attr('src', '/img/icons/left-hover.png')
+        $(this).find('img').attr('src', './img/icons/left-hover.png')
         $(this).addClass('active');
     })
 
     $rightNav.find('> li').eq(i).on('mouseleave', function(){
-        $(this).find('img').attr('src', '/img/icons/left-stroke.png')
+        $(this).find('img').attr('src', './img/icons/left-stroke.png')
         $(this).removeClass('active');
     })
-
 }
 
